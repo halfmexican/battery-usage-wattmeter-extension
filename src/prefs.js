785deebfb46e24c7746bacd1cdeba0ac66ae1ff4
halfmexican <103920890+halfmexican@users.noreply.github.com>
2023-09-25 10:25:42 -0500

    make settings local to window

--- src/prefs.js
+++ src/prefs.js
@@ -1,20 +1,19 @@
-// Import necessary modules
 import Gtk from 'gi://Gtk';
 import Gio from 'gi://Gio';
 import Adw from 'gi://Adw';
 import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 export default class MyExtensionPreferences extends ExtensionPreferences {
-  fillPreferencesWindow(prefWindow) {
-    // Initialize settings
-    this._settings = this.getSettings('org.gnome.shell.extensions.battery_usage_wattmeter');
+  fillPreferencesWindow(window) {
+    // Attach settings to window
+    window._settings = this.getSettings('org.gnome.shell.extensions.battery_usage_wattmeter');
 
     // Create a preferences page
     const generalPage = new Adw.PreferencesPage({
       title: _('General'),
       icon_name: 'dialog-information-symbolic',
     });
-    prefWindow.add(generalPage);
+    window.add(generalPage);
 
     // Create a preferences group titled 'Behavior'
     const behaviorGroup = new Adw.PreferencesGroup({
@@ -35,7 +34,7 @@ export default class MyExtensionPreferences extends ExtensionPreferences {
       }),
     });
     behaviorGroup.add(intervalRow);
-    this._settings.bind('interval', intervalRow, 'value', Gio.SettingsBindFlags.DEFAULT);
+    window._settings.bind('interval', intervalRow, 'value', Gio.SettingsBindFlags.DEFAULT);
 
     // Create a StringList and populate it with battery options
     const batteryStringList = new Gtk.StringList();
@@ -48,6 +47,6 @@ export default class MyExtensionPreferences extends ExtensionPreferences {
     });
     behaviorGroup.add(batteryRow);
 
-    this._settings.bind('battery', batteryRow, 'selected', Gio.SettingsBindFlags.DEFAULT);
+    window._settings.bind('battery', batteryRow, 'selected', Gio.SettingsBindFlags.DEFAULT);
   }
 }
