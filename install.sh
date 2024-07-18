c8344f666fda819301cb21050566343c4b4fc67d
half_mexican <halfmexicanhalfamazing@gmail.com>
2024-07-18 04:13:54 -0500

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gnome-extensions pack --force
+gnome-extensions install battery-usage-wattmeter@halfmexicanhalfamazing.gmail.com.shell-extension.zip --force
+rm battery-usage-wattmeter@halfmexicanhalfamazing.gmail.com.shell-extension.zip
