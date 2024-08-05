ee06d220db50529d709f98e56901bfa0ef243359
half_mexican <halfmexicanhalfamazing@gmail.com>
2024-08-05 10:59:59 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Define the extension name and zip file
+extension_name="battery-usage-wattmeter@halfmexicanhalfamazing.gmail.com"
+extension_zip="${extension_name}.shell-extension.zip"
+extension_dir="$HOME/.local/share/gnome-shell/extensions/$extension_name"
+
+# Pack the extension
 gnome-extensions pack --force
-gnome-extensions install battery-usage-wattmeter@halfmexicanhalfamazing.gmail.com.shell-extension.zip --force
-rm battery-usage-wattmeter@halfmexicanhalfamazing.gmail.com.shell-extension.zip
+
+# Install the extension
+gnome-extensions install "$extension_zip" --force
+
+# Remove the zip file
+rm "$extension_zip"
+
+# Check if the extension folder exists
+if [ -d "$extension_dir" ]; then
+    echo "Extension installed successfully."
+else
+    echo "Extension installation failed."
+fi
+
+
